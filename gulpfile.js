f4c814a8cdc5bd439a748a18bde250da75548e2e
Xavier Garcia Buils <xgbuils@gmail.com>
2015-10-24 11:54:48 +0200

    fix travis-ci

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp  = require('gulp')
 var mocha = require('gulp-mocha')
 var eslint = require('gulp-eslint')
-var karma = require('karma').Server
 
 gulp.task('test', function () {
     gulp.src('./test/tree-measurer_test.js')
